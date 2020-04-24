dd9f6755e83ac99067ccf437b07b1809dd697e59
Anthony Durham <durhamusa1313@gmail.com>
2020-04-24 11:46:36 -0500

    intial

---
+++ CriticalThinkingproj/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopExamples loops = new LoopExamples();
+            loops.ShowWordsInWord("Cowboys");
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+        
+        
+
+            
+        
+    
+}
